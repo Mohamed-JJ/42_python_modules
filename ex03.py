31497968c353aadbbbeaf7cfb53f5c406da69557
Mohamed Jarboua <mjarboua@e1r5p5.1337.ma>
2023-05-25 21:55:32 +0100

    made the code  bettere with some improvments

--- ex03.py
+++ ex03.py
@@ -1,8 +1,13 @@
-def text_analyser(string):
+def text_analyser(string=None):
+    """
+    This function counts the number of upper characters, lower characters, punctuation and spaces in a given text.
+    """
     upper_case = 0
     lower_case = 0
     punctuation = 0
     spaces = 0
+    if string == None:
+        string = input("What is the text to analyse?\n")
     for i in string:
         if i.isupper():
             upper_case += 1
@@ -12,4 +17,8 @@ def text_analyser(string):
             punctuation += 1
         elif i.isspace():
             spaces += 1
-    return upper_case, lower_case, punctuation, spaces
+    print("The text contains", len(string))
+    print("The amount of upper characters is", upper_case)
+    print("The amount of lower characters is", lower_case)
+    print("The amount of punctuation characters is", punctuation)
+    print("The amount of spaces is", spaces)
